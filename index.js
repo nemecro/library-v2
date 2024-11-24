974ffaf550c967f430a380e0fd9d16dbcf7b2fef
nemecro <nemecstudies@gmail.com>
2024-11-24 16:15:31 +0100

    feat: add changeStatus method that when called changes the status to either true or false

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ class Book {
             alert('Invalid input');
         }
     }
+
+    changeStatus(){
+        if (this.status === true){
+            this.status = false;
+        } else {
+            this.status = true;
+        }
+    }
 }
 
 const book1 = new Book('Title', 'Author', 329, true);
+console.log(book1);
+book1.changeStatus();
 console.log(book1);
