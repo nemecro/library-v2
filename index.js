f93d50fc3c177264f71b39615e454aa41bb01531
nemecro <nemecstudies@gmail.com>
2024-11-24 16:11:01 +0100

    feat: create class Book with a constructor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello, world');
+class Book {
+    constructor(title, author, pages, status){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+}
+
+const book1 = new Book('Title', 'Author', 329, 'read');
+console.log(book1);
